29598c0370d8eccf1e5d9335421a9b023fc693fd
shivashankar1253 <65164540+shivashankar1253@users.noreply.github.com>
2021-03-05 15:59:26 +0530

    Add files via upload

---
+++ student2.js
@@ -0,0 +1,8 @@
+let  students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+]
+let obj = Object.fromEntries(students)
+//let obj = Object.assign({}, students);
+console.log(obj)
+
