e886103c3dc0a8381854ba92b62551e003c231b0
shivashankar1253 <65164540+shivashankar1253@users.noreply.github.com>
2021-03-05 14:45:58 +0530

    Add files via upload

---
+++ students.js
@@ -0,0 +1,5 @@
+let student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+let [name, skills, marks] = student;
+console.log("name:",student[0]) 
+console.log("skills:",skills)
+console.log("marks:", marks)
